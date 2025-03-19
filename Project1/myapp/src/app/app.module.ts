7bc5e54fde4dfe92d6d503450083c716f1d4446c
Suhas Gondukupi <suhas8838@gmail.com>
2025-03-19 17:25:42 +0530

    Login/Register

--- Project1/myapp/src/app/app.module.ts
+++ Project1/myapp/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProductComponent } from './product/product.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,   //added for two way data bindling
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Project1/myapp/src/app/register/register.component.ts
+++ Project1/myapp/src/app/register/register.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  //since we have not give value to variable therefore give !
+  name !: string;
+  email !: string; 
+  pass !: string;
+  cpass !: string;
 
+  //we use two way binding
+  //1. app.ts add
+  //2. [(ngModel)='var_name']
+
+  
+
+  doRegister(){
+    console.log('Register done')
+    console.log(this.name)
+  }
 }
