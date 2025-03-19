58dcc6fed619ee3477f71098418eb0b8be76aa17
Suhas Gondukupi <suhas8838@gmail.com>
2025-03-19 11:56:24 +0530

    routerLink

--- Project1/myapp/src/app/app.module.ts
+++ Project1/myapp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProductComponent } from './product/product.component';
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    ProductComponent
+    ProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
